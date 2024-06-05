b267d55d9d5be2dfdac9b3a07b573a86d280ba6d
jihen Skhiri <jihen.skhiri@gmail.com>
2024-06-05 17:16:21 +0200

    présentation-travaux

--- FrontEnd/assets/gallery.js
+++ FrontEnd/assets/gallery.js
@@ -1,6 +1,8 @@
 
 const reponse = await fetch('http://localhost:5678/api/works')
 const projects = await reponse.json();
+const reponseCategorie = await fetch('http://localhost:5678/api/categories')
+const categories = await reponseCategorie.json();
 /************Create Gallery************ */
 function AfficheGallery(projects){ 
 for(let i=0; i<projects.length; i++) {
@@ -9,6 +11,7 @@ for(let i=0; i<projects.length; i++) {
     const image = document.createElement("img");
     image.src = projects[i].imageUrl;
     image.alt =projects[i].title;
+    figure.dataset.category = projects[i].category.name;
     figure.appendChild(image);
     const figcaption = document.createElement("figcaption");
     figcaption.innerHTML = projects[i].title;
@@ -18,5 +21,4 @@ for(let i=0; i<projects.length; i++) {
 }
 AfficheGallery(projects)
 
-/*********Create Filter&Buttons******* */
-filter = document.querySelector(".filter");
+
